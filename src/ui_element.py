4eb54c20ad7065833cab4d8ab4867897cb56ff26
lyon <ang@simular.ai>
2024-08-02 16:28:17 -0700

    initial commit for UIElement

---
+++ src/ui_element.py
@@ -0,0 +1,53 @@
+from Foundation import *
+from AppKit import *
+
+from ApplicationServices import (
+    AXIsProcessTrusted,
+    AXUIElementCreateApplication,
+    AXUIElementCreateSystemWide,
+    CFEqual,
+)
+
+from ApplicationServices import (
+    AXUIElementCopyAttributeNames,
+    AXUIElementCopyAttributeValue,
+)
+
+
+def frontmostApplication():
+    return NSWorkspace.sharedWorkspace().frontmostApplication()
+
+
+class UIElement(object):
+
+    def __init__(self, ref=None):
+        self.ref = ref
+
+    def getAttributeNames(self):
+        error_code, attributeNames = AXUIElementCopyAttributeNames(
+            self.ref, None)
+        return list(attributeNames)
+
+    def attribute(self, key: str):
+        error, value = AXUIElementCopyAttributeValue(self.ref, key, None)
+        return value
+
+    def children(self):
+        return self.attribute('AXChildren')
+
+    def systemWideElement():
+        ref = AXUIElementCreateSystemWide()
+        return UIElement(ref)
+
+    def __repr__(self):
+        return "UIElement%s" % (self.ref)
+
+
+if __name__ == "__main__":
+    # Examples.
+    elem = UIElement.systemWideElement()
+    print(elem)
+    print(elem.getAttributeNames())
+    elem = (UIElement(elem.attribute('AXFocusedApplication')))
+    print(elem.getAttributeNames())
+print(elem.children())
